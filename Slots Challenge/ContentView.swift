d9e38692294cd74b70ff1b2029ac43ac4f43fe83
Dharamveer Gupta <dharamveer.gupt@gmail.com>
2021-12-06 21:01:48 +0530

    Initial Commit

---
+++ Slots Challenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slots Challenge
+//
+//  Created by Dharamveer Gupta on 06/12/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slots Challenge/Slots_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Slots_ChallengeApp.swift
+//  Slots Challenge
+//
+//  Created by Dharamveer Gupta on 06/12/21.
+//
+
+import SwiftUI
+
+@main
+struct Slots_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
