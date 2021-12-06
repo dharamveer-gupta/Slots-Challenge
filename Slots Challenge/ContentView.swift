3b1e83671bacf4dd8a2595003cbe36f011996ee4
Dharamveer Gupta <dharamveer.gupt@gmail.com>
2021-12-06 21:48:33 +0530

    Initial Commit with CodeWithChris Challenge Solution

--- Slots Challenge/ContentView.swift
+++ Slots Challenge/ContentView.swift
@@ -8,9 +8,58 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var credits = 1000
+    @State var slot1 = 1
+    @State var slot2 = 1
+    @State var slot3 = 1
+    
     var body: some View {
-        Text("Hello, world!")
+        
+        VStack(spacing: 20.0) {
+            Spacer()
+            Text("SwiftUI Slots!").font(.largeTitle)
+            Spacer()
+            Text("Credit \(credits)")
+            Spacer()
+            HStack {
+                // The images were too wide to begin with so make sure you
+                // add the resizable and aspectRatio modifiers
+                Image("fruit\(slot1)")
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fit)
+                Image("fruit\(slot2)")
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fit)
+                Image("fruit\(slot3)")
+                    .resizable()
+                    .aspectRatio(contentMode: ContentMode.fit)
+            }
+            Spacer()
+            Button("Spin") {
+                // Randomize the numbers
+                slot1 = Int.random(in: 1...3)
+                slot2 = Int.random(in: 1...3)
+                slot3 = Int.random(in: 1...3)
+                
+                // Update credits based on match or not
+                if slot1 == slot2 && slot2 == slot3 {
+                    // Match
+                    credits += 15
+                } else {
+                    credits -= 5
+                }
+            }
+            // Set padding for all edges
             .padding()
+            // Then adjust the left and right padding to be bigger
+            .padding([.leading, .trailing], 40)
+            .foregroundColor(.white)
+            .background(Color(.systemPink))
+            .cornerRadius(25)
+            .font(.system(size: 18, weight: .bold, design: .default))
+            Spacer()
+        }
     }
 }
 
